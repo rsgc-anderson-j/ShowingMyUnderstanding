ea95201d5bb7d3cfc2993868b1eaf907d8bfa34c
Student <student@MacBook-Pro-856-Anderson.local>
2017-02-26 08:00:04 -0500

    made my own logo

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,8 +21,37 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
+
 let canvas = Canvas(width: 300, height: 300)
 
+
+
+//making the ears
+canvas.drawEllipse(centreX: 100, centreY: 210, width: 50, height: 50, borderWidth: 1)
+canvas.drawEllipse(centreX: 200, centreY: 210, width: 50, height: 50, borderWidth: 1)
+
+//drawing the first circle
+canvas.fillColor = Color.orange
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 150, height: 150, borderWidth: 1)
+
+//the nose on the panda
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 150, centreY: 130, width: 20, height: 20, borderWidth: 1)
+
+//eyes on the panda
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 115, centreY: 170, width: 40, height: 40, borderWidth: 1)
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 185, centreY: 170, width: 40, height: 40, borderWidth: 1)
+
+//middle of the eyes
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 110, centreY: 160, width: 10, height: 10, borderWidth: 1)
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 175, centreY: 160, width: 10, height: 10, borderWidth: 1)
+
+//make the mouth
+canvas.drawLine(fromX: 120, fromY: 100, toX: 180, toY: 100, lineWidth: 5)
 // Replace this comment and add your code below...
 
 /*:
