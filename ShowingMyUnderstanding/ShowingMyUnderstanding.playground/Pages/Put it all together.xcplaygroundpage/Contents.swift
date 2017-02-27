413397cb77d5ef80191283228b1e021a3eb44af4
Student <student@MacBook-Pro-856-Anderson.local>
2017-02-27 10:50:54 -0500

    finished formative assessment

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Put it all together.xcplaygroundpage/Contents.swift
@@ -21,8 +21,41 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
 
-// Replace this comment and add your code below...
+let number = random(from: 0, toButNotIncluding: 15)
 
+if number >= 10 {
+    canvas.fillColor = Color.black
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 300, height: 300, borderWidth: 1)
+    for x in stride(from: 10, through: 100, by: 10) {
+        canvas.borderColor = Color.black
+        canvas.drawEllipse(centreX:   150, centreY: 150, width: x, height: x, borderWidth: 1)
+        }
+    //red circle in the bottom left
+    for x in stride(from: 10, through: 100, by: 10) {
+        canvas.borderColor = Color.red
+        canvas.drawEllipse(centreX:   100, centreY: 100, width: x, height: x, borderWidth: 1)
+        }
+    //orange circle in the top right
+    for x in stride(from: 10, through: 100, by: 10) {
+        canvas.borderColor = Color.orange
+        canvas.drawEllipse(centreX:   200, centreY: 200, width: x, height: x, borderWidth: 1)
+        }
+    //blue circle in the bottom right
+    for x in stride(from: 10, through: 100, by: 10) {
+        canvas.borderColor = Color.blue
+        canvas.drawEllipse(centreX:   200, centreY: 100, width: x, height: x, borderWidth: 1)
+        }
+    //green circle in the top left
+    for x in stride(from: 10, through: 100, by: 10) {
+        canvas.borderColor = Color.green
+        canvas.drawEllipse(centreX:   100, centreY: 200, width: x, height: x, borderWidth: 1)
+    }
+}
+
+else if number <= 5 {
+    canvas.fillColor = Color.orange
+    canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 100, height: 100, borderWidth: 1)
+}
 
 /*:
  ## Template code
